c0cd351afb76d924c05b0e898987bb5bba2aa7b1
Jiwon <yesjiwon5304@gmail.com>
2021-12-17 23:08:42 +0900

    [feature #6] movie detail and list - 영화 상세조회 - 영화 리스트조회

--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,89 @@
-import json 
+import json
 
-from django.http    import JsonResponse
-from django.views   import View
+from django.http        import JsonResponse
+from django.views       import View 
+from django.db.models   import Q
 
-from core.decorator import login_decorator
-from users.models   import User
-from movies.models  import Movie, Recommend, Review
+from core.decorator     import login_decorator
+from users.models       import User
+from movies.models      import Movie, Recommend, Review
+
+# 1. 영화 리스트조회 API
+class MovieListView(View):
+    @login_decorator
+    def get(self, request):
+        try:
+            limit  = int(request.GET.get("limit","10"))
+            offset = int(request.GET.get("offset","0"))
+
+            genres         = request.GET.get("genres", None)
+            year           = request.GET.get("year", None)
+            search_keyword = request.GET.get("keyword", None)
+            rating         = request.GET.get("rating", None)
+
+            q = Q()
+
+            if genres:
+                q &= Q(genres__icontains=genres)
+
+            if year:
+                q &= Q(year=year)
+
+            if search_keyword:
+                q &= Q(title__icontains=search_keyword)
+            
+            all_movies = Movie.objects.filter(q)
+            
+            if rating == "up":
+                all_movies = all_movies.order_by('rating')
+
+            if rating == "down":
+                all_movies = all_movies.order_by('-rating')
+
+            offset  = offset*limit
+            movies  = all_movies[offset:offset+limit]
+
+            movies_list = [{
+                "id"      : movie.id,
+                "title"   : movie.title,
+                "year"    : movie.year,
+                "rating"  : movie.rating,
+                "genres"  : movie.genres,
+                "summary" : movie.summary,
+            }for movie in movies]
+
+            result = {
+                "count": len(movies_list),
+                "data" : movies_list
+            }
+            return JsonResponse({"results": result}, status=200)
+
+        except Movie.DoesNotExist:
+            return JsonResponse({"MESSAGE": "DOES_NOT_EXIST_MOVIE"}, status=404)
+
+
+# 2. 영화 상세조회 API
+class MovieDetailView(View):
+    @login_decorator
+    def get(self, request, movie_id):
+        try:
+            reviews = Review.objects.filter(movie_id=movie_id)
+            review_list = [review.text for review in reviews]
+
+            movie = Movie.objects.get(id=movie_id)
+
+            result = {
+                "id"      : movie.id,
+                "title"   : movie.title,
+                "rating"  : movie.rating,
+                "genres"  : movie.genres,
+                "summary" : movie.summary,
+                "review"  : review_list
+            }
+            return JsonResponse({"results": result}, status=200)
+
+        except Movie.DoesNotExist:
+            return JsonResponse({"MESSAGE": "DOES_NOT_EXIST_MOVIE"}, status=404)
 
 # 3. 영화 생성 API
 class MovieCreateView(View):
@@ -24,7 +102,7 @@ class MovieCreateView(View):
                 title     = data["title"],
                 year      = data["year"],
                 rating    = 0.0,
-                genres    = data["year"],
+                genres    = data["genres"],
                 summary   = data["summary"]
                 )
             return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)

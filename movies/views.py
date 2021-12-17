38995998a631781a723630e3e267938858cd8514
Jiwon <yesjiwon5304@gmail.com>
2021-12-17 17:01:30 +0900

    [feature #4] create movie - 영화 생성 기능 구현

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import json 
 
-# Create your views here.
+from django.http    import JsonResponse
+from django.views   import View
+
+from core.decorator import login_decorator
+from users.models   import User
+from movies.models  import Movie, Recommend, Review
+
+class MovieCreateView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if request.user.is_admin == False:
+                return JsonResponse({"MESSAGE": "Only administrators can access"}, status=401)
+                
+            if data["title"] == '' or data["year"] == '' or data["genres"] == ''or data["summary"] == '':
+                return JsonResponse({"MESSAGE": "INPUT_ERROR"}, status=400)
+            
+            Movie.objects.create(
+                    title     = data["title"],
+                    year      = data["year"],
+                    rating    = 0.0,
+                    genres    = data["year"],
+                    summary   = data["summary"]
+                )
+            return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
